6bdcc433585124f525f1f1ff27c476e87f2a1c77
tatsuya <clarkmaster@icloud.com>
2024-09-06 00:16:41 +0900

    ボタン設定

---
+++ frontend/src/app/components/atoms/Button/Index.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react';
+import styles from './index.module.scss';
+
+interface ButtonProps {
+  variant: string;
+  Children?: ReactNode;
+  [key: string]: any;
+}
+
+const Button = ({ variant = 'base', children, ...props }: ButtonProps) => {
+  const className = `${styles.button} ${styles[variant]}`;
+  return (
+    <button className={className} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;

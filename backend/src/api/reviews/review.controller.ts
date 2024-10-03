8eedc75a6837b11e3b7c46d662c1ef3be1531547
tatsuya <clarkmaster@icloud.com>
2024-10-03 22:23:29 +0900

    レビュー機能仮実装

--- backend/src/api/reviews/review.controller.ts
+++ backend/src/api/reviews/review.controller.ts
@@ -1,10 +1,62 @@
 // HTTPレスポンスとリクエストの処理を記述
 
-import { Request, Response } from "express";
-import reviewModel from "./review.model";
+import { Request, Response, NextFunction } from 'express';
+import reviewModel, { reviewSchema } from './review.model';
+import { Review } from '@prisma/client';
+import { AppError } from '../../middleware/errorHandler';
 
-export const create = async (req: Request, res: Response) => {
-  const reviewData = req.body;
-  const newReview = await reviewModel.createReview(reviewData);
-  res.status(201).json(newReview);
+// レビュー作成
+export const create = async (req: Request, res: Response, next: NextFunction): Promise<Review | void> => {
+  const parseReview = await reviewSchema.safeParse(req.body);
+  if (!parseReview.success) {
+    return next(new AppError(parseReview.error.message, 400));
+  }
+  const reviewData = parseReview.data;
+  try {
+    const newReview = await reviewModel.createReview(reviewData);
+    res.status(201).json(newReview);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// レビュー獲得
+export const get = async (req: Request, res: Response, next: NextFunction) => {
+  const reviewId = await parseInt(req.params.id, 10);
+  const review = await reviewModel.getReviewById(reviewId);
+  try {
+    if (!review) {
+      throw new AppError('レビューが見つかりません', 400);
+    }
+    res.json(review);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// レビュー更新
+export const update = async (req: Request, res: Response, next: NextFunction) => {
+  const reviewId = await parseInt(req.params.id, 10);
+  const parseReview = await reviewSchema.safeParse(req.body);
+  if (!parseReview.success) {
+    throw new AppError(parseReview.error.message, 400);
+  }
+  const review = parseReview.data;
+  try {
+    const updateReview = await reviewModel.updateReview(reviewId, review);
+    res.json(updateReview);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// レビュー削除
+export const remove = async (req: Request, res: Response, next: NextFunction) => {
+  const reviewId = await parseInt(req.params.id);
+  try {
+    await reviewModel.deleteReview(reviewId);
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
 };
--- backend/src/api/reviews/review.routes.ts
+++ backend/src/api/reviews/review.routes.ts
@@ -1,11 +1,13 @@
 // APIエンドポイントの定義とルーティングを記述
 
-import express from "express";
-import { create } from "./review.controller";
-import { validate } from "./review.middleware";
+import express from 'express';
+import { create, get, remove, update } from './review.controller';
 
 const router = express.Router();
 
-router.post("/", validate, create);
+router.post('/', create);
+router.get('/:id', get);
+router.put('/:id', update);
+router.delete('/:id', remove);
 
 export default router;

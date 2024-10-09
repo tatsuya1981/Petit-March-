adc893e06733568181496091fe5c42c99fdeb49a
tatsuya <clarkmaster@icloud.com>
2024-10-09 15:41:23 +0900

    星評価の作成完了 #69

--- frontend/features/review/components/rating/index.tsx
+++ frontend/features/review/components/rating/index.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { useState } from 'react';
 import styles from './index.module.scss';
+import { Star } from 'lucide-react';
 
 export const Rating = () => {
   const [rating, setRating] = useState(0);
@@ -11,7 +14,32 @@ export const Rating = () => {
         <div className={styles.textContainer}>
           <p className={styles.text}>星評価（満点は星５つ）</p>
         </div>
-        <div className={styles.ratingContainer}></div>
+        <div className={styles.ratingContainer}>
+          {[...Array(5)].map((_, index) => {
+            const ratingValue = index + 1;
+            return (
+              <label key={index}>
+                <input
+                  className={styles.starInput}
+                  type="radio"
+                  name="rating"
+                  value={ratingValue}
+                  onClick={() => setRating(ratingValue)}
+                />
+                <Star
+                  className={styles.starIcon}
+                  size={52}
+                  color={'#3C2A21'}
+                  fill={ratingValue <= (hover || rating) ? '#ffc107' : 'none'}
+                  onMouseEnter={() => setHover(ratingValue)}
+                  onMouseLeave={() => setHover(0)}
+                  style={{ marginLeft: index === 0 ? '0' : '2rem' }}
+                />
+              </label>
+            );
+          })}
+        </div>
+        <p>選択した評価: {rating}</p>
       </div>
     </>
   );

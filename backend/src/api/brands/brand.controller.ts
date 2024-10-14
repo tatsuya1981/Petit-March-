09c3681e2a73f1a8e83bb6d74a88de063316e0a2
tatsuya <clarkmaster@icloud.com>
2024-10-15 01:05:00 +0900

    リスト表示をデータベースから取得修正 #77

--- backend/src/api/brands/brand.controller.ts
+++ backend/src/api/brands/brand.controller.ts
@@ -1,24 +1,32 @@
 // HTTPレスポンスとリクエストの処理を記述
 
-import { Request, Response } from "express";
-import prisma from "../../config/database";
+import { NextFunction, Request, Response } from 'express';
+import prisma from '../../config/database';
 
 // コンビニブランドカテゴリー取得
-export const get = async (req: Request, res: Response) => {
-  const categories = await prisma.brand.findMany({
-    orderBy: {
-      // 昇順に並べる
-      name: "asc",
-    },
-  });
-  res.json(categories);
+export const get = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const categories = await prisma.brand.findMany({
+      orderBy: {
+        // 昇順に並べる
+        brandId: 'asc',
+      },
+    });
+    res.json(categories);
+  } catch (error) {
+    next(error);
+  }
 };
 
 // コンビニブランドカテゴリー作成
-export const create = async (req: Request, res: Response) => {
-  const { name, brandId } = req.body;
-  const category = await prisma.brand.create({
-    data: { name, brandId },
-  });
-  res.status(201).json(category);
+export const create = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { name, brandId } = req.body;
+    const category = await prisma.brand.create({
+      data: { name, brandId },
+    });
+    res.status(201).json(category);
+  } catch (error) {
+    next(error);
+  }
 };
--- frontend/features/review/components/productInfo/index.tsx
+++ frontend/features/review/components/productInfo/index.tsx
@@ -1,41 +1,56 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './index.module.scss';
 import FormSelect from '@/components/elements/formSelect';
 import FormInput from '@/components/elements/formInput';
 import DatePick from '@/components/elements/datePicker';
+import axios from 'axios';
+
+interface Option {
+  value: string;
+  label: string;
+}
 
 const ProductInfo = () => {
+  const [categoryOptions, setCategoryOptions] = useState<Option[]>([]);
+  const [brandOptions, setBrandOptions] = useState<Option[]>([]);
   const [category, setCategory] = useState('');
   const [productName, setProductName] = useState('');
   const [price, setPrice] = useState('');
   const [purchaseDate, setPurchaseDate] = useState<Date | null>(null);
   const [brand, setBrand] = useState('');
 
-  const categoryOptions = [
-    { value: '1', label: 'お弁当・おにぎり' },
-    { value: '2', label: 'パン・サンド' },
-    { value: '3', label: '麺類' },
-    { value: '4', label: '惣菜' },
-    { value: '5', label: 'ホットスナック' },
-    { value: '6', label: 'スイーツ' },
-    { value: '7', label: 'お菓子' },
-    { value: '8', label: 'インスタント食品' },
-    { value: '9', label: 'ドリンク類' },
-    { value: '10', label: 'その他' },
-  ];
+  useEffect(() => {
+    const fetchCategoryOptions = async () => {
+      try {
+        const response = await axios.get('http://localhost:4000/api/products/');
+        const options = response.data.map((product: { productId: number; name: string }) => ({
+          value: product.productId.toString(),
+          label: product.name,
+        }));
+        setCategoryOptions(options);
+      } catch (error) {
+        console.error('Error fetching categories', error);
+      }
+    };
 
-  const brandOptions = [
-    { value: '1', label: 'セブンイレブン' },
-    { value: '2', label: 'ファミリーマート' },
-    { value: '3', label: 'ローソン' },
-    { value: '4', label: 'ミニストップ' },
-    { value: '5', label: 'デイリーヤマザキ' },
-    { value: '6', label: 'セイコーマート' },
-    { value: '7', label: 'さくらみくら' },
-    { value: '8', label: 'その他' },
-  ];
+    const fetchBrandOptions = async () => {
+      try {
+        const response = await axios.get('http://localhost:4000/api/brands/');
+        const options = response.data.map((brand: { brandId: number; name: string }) => ({
+          value: brand.brandId.toString(),
+          label: brand.name,
+        }));
+        setBrandOptions(options);
+      } catch (error) {
+        console.error('Error fetching brands', error);
+      }
+    };
+
+    fetchCategoryOptions();
+    fetchBrandOptions();
+  }, []);
 
   return (
     <div className={styles.productContainer}>

9068493bc11d168ebd775b31d6b6d5304171c1a9
tatsuya <clarkmaster@icloud.com>
2024-11-12 23:39:47 +0900

    Googleログインを仮表示

--- frontend/app/(routes)/sign-up/page.tsx
+++ frontend/app/(routes)/sign-up/page.tsx
@@ -1,6 +1,9 @@
 import styles from './page.module.scss';
 import { SignUpTitle } from '../../../features/sign-up/components/title';
 import { SignUpForm } from '../../../features/sign-up/components/userInfo';
+import dynamic from 'next/dynamic';
+
+const GoogleAuthButton = dynamic(() => import('@/components/elements/googleAuthButton'), { ssr: false });
 
 const Home = () => {
   return (
@@ -8,6 +11,8 @@ const Home = () => {
       <main className={styles.main}>
         <SignUpTitle />
         <SignUpForm />
+        <div className={styles.divider}>または</div>
+        <GoogleAuthButton className={styles.googleButton} />
       </main>
     </>
   );
